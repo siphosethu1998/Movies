0547f9edd9e3e2298a9b5b5aa572f90274c0d6cc
siphosethu1998 <siphosethu1998@gmail.com>
2022-12-09 16:31:56 +0200

    Page display user friendly 404y

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#_2!#+y8d*4)0qdxdsiuotdew5nmdv0)hr6r5hzn-zcmxx29=0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
